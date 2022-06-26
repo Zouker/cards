edd90a7920a092f17938ca878e75d66f367a34c2
Denis Shubin <denis.shubin@gmail.com>
2022-06-26 15:32:34 +0300

    feat: fix routes and add error page 404

--- src/n1-main/m1-ui/routes/Pages.tsx
+++ src/n1-main/m1-ui/routes/Pages.tsx
@@ -1,19 +1,23 @@
-import {Route, Routes} from 'react-router-dom';
+import {Navigate, Route, Routes} from 'react-router-dom';
 import React from 'react';
 import {Register} from '../../../n2-features/f2-register/Register';
+import {RecoverPassword} from '../../../n2-features/f4-recover-password/RecoverPassword';
 
 export const Pages = () => {
 
     return (
         <Routes>
-            <Route path={'/'} element={<div>Main</div>}></Route>
-            <Route path={'/login'} element={<div>Login</div>}></Route>
-            <Route path={'/register'} element={<div><Register/></div>}></Route>
-            <Route path={'/profile'} element={<div>Profile</div>}></Route>
-            <Route path={'/error404'} element={<div>Error404</div>}></Route>
-            <Route path={'/recoverPassword'} element={<div>Recover Password</div>}></Route>
-            <Route path={'/enterNewPassword'} element={<div>Enter New Password</div>}></Route>
-            <Route path={'/test'} element={<div>Test</div>}></Route>
+            <Route path={'/'} element={<Navigate to={'/login'}/>}/>
+            <Route path={'/login'} element={<div>Login</div>}/>
+            <Route path={'/register'} element={<div><Register/></div>}/>
+            <Route path={'/profile'} element={<div>Profile</div>}/>
+            <Route path={'*'} element={<Navigate to={'404'}/>}/>
+            <Route path={'/404'} element={<h1 style={{display: 'flex', justifyContent: 'center'}}>
+                404: PAGE NOT FOUND
+            </h1>}/>
+            <Route path={'/recoverPassword'} element={<div><RecoverPassword/></div>}/>
+            <Route path={'/enterNewPassword'} element={<div>Enter New Password</div>}/>
+            <Route path={'/test'} element={<div>Test</div>}/>
         </Routes>
     )
 }
