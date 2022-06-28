50d78eeb28bc1e11271bd237a25b8aa2a4a13600
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-06-28 19:48:38 +0300

    1

--- src/n1-main/m2-bll/reducers/profile-reducer.ts
+++ src/n1-main/m2-bll/reducers/profile-reducer.ts
@@ -1,4 +1,45 @@
+import {registerAC, setErrorAC} from "./register-reducer";
+import {RegDataType, registerAPI} from "../../m3-dal/registerAPI";
+import {AppDispatch} from "../store";
 
-export const profileReducer = (state: any={}, action: any): any => {
-    return state
+
+type InitialStateType = {
+    isRegistered: boolean
+    error: string | null
 }
+
+const initialState: InitialStateType = {
+    isRegistered: false,
+    error: null
+}
+
+export const profileReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case "IS-SIGNED-UP":
+            return {...state, isRegistered: action.isRegistered}
+        default:
+            return state
+    }
+}
+
+// actions
+export const profileAC = (isRegistered: boolean) => ({type: 'IS-SIGNED-UP', isRegistered} as const)
+
+// thunks
+export const profileTC = (regData: RegDataType) => {
+    return (dispatch: AppDispatch) => {
+        registerAPI.register(regData)
+            .then(() => {
+                dispatch(registerAC(true))
+            })
+            .catch((error) => {
+                if (error.response) {
+                    dispatch(setErrorAC(error.response.data.error))
+                }
+            })
+    }
+}
+
+
+// types
+type ActionsType = ReturnType<typeof profileAC> | ReturnType<typeof setErrorAC>
--- src/n2-features/f3-profile/Profile.tsx
+++ src/n2-features/f3-profile/Profile.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from "./Profile.module.css"
+import {Navigate} from "react-router-dom";
+import {useAppDispatch, useAppSelector} from "../../n1-main/m2-bll/store";
 
 
 
 const Profile = () => {
-
+    const dispatch = useAppDispatch()
+    const isRegistered = useAppSelector(state => state.register.isRegistered)
+    if (!isRegistered) {
+        return <Navigate to={'/login'}/>
+    }
     return (
         <div className={styles.wrapper}>
             <div className={styles.profileImg}>
