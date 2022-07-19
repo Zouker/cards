4c54549cfd063c6e9435081f6445c7e196863b88
Denis Shubin <denis.shubin@gmail.com>
2022-07-19 12:17:59 +0300

    fix: naming

--- src/n2-features/f7-cards/Modals/DeleteCardModal.tsx
+++ src/n2-features/f7-cards/Modals/DeleteCardModal.tsx
@@ -3,30 +3,30 @@ import {BasicModal} from '../../../n1-main/m1-ui/common/c7-Modal/Modal';
 import {useAppDispatch} from '../../../n1-main/m2-bll/store';
 import {deleteCardTC} from '../../../n1-main/m2-bll/reducers/cards-reducer';
 
-type DeletePackModalType = {
+type DeleteCardType = {
     cardQuestion: string
-    deleteCardPackButton: ReactNode
+    deleteCardButton: ReactNode
     cardId: string
     packId: string
 }
 
-export const DeleteCardModal: React.FC<DeletePackModalType> = ({
-                                                                   cardId,
-                                                                   packId,
-                                                                   cardQuestion,
-                                                                   deleteCardPackButton
-                                                               }) => {
+export const DeleteCardModal: React.FC<DeleteCardType> = ({
+                                                              cardId,
+                                                              packId,
+                                                              cardQuestion,
+                                                              deleteCardButton
+                                                          }) => {
     const dispatch = useAppDispatch()
 
-    const deleteCardPack = () => {
+    const deleteCard = () => {
         dispatch(deleteCardTC(cardId, packId))
     }
 
     return (
         <BasicModal operationTitle={'Delete Card'}
                     buttonName={'Delete'}
-                    handleOperation={deleteCardPack}
-                    openModalButton={deleteCardPackButton}
+                    handleOperation={deleteCard}
+                    openModalButton={deleteCardButton}
         >
             <div>Do you really want to remove <b>{cardQuestion}</b>?</div>
             <div>The card will be removed.</div>
