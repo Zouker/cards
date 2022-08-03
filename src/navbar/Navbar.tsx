b6a7b4ec762a64542eb01e63ca39668dff9051a5
Denis Shubin <denis.shubin@gmail.com>
2022-08-03 20:25:29 +0300

    refactor: add highlight button for profile or pack

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -9,19 +9,18 @@ export const Navbar = () => {
         <div className={styles.wrapper}>
             <div className={styles.container}>
                 <div className={styles.itemForm}>
-                    <NavLink to={'/packs'} className={styles.item}>
+                    <NavLink to={'/packs'} className={({isActive}) => isActive ? styles.active : styles.item}>
                         <img src={packList} className={styles.packList} alt={'Packs list icon'}/>
                         <span>Packs list</span>
                     </NavLink>
                 </div>
                 <div className={styles.itemForm}>
-                    <NavLink to={'/profile'} className={styles.item}>
+                    <NavLink to={'/profile'} className={({isActive}) => isActive ? styles.active : styles.item}>
                         <img src={profile} className={styles.profile} alt={'Profile icon'}/>
                         <span>Profile</span>
                     </NavLink>
                 </div>
             </div>
         </div>
-
     );
 };
